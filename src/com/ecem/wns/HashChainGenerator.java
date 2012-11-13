3159480b8204eb6f1c9af072fb0b7d96320e5961
Ecem Unal <ecunal@gmail.com>
2012-11-13 17:23:40 +0200

    Binary hash chain generator added

--- src/com/ecem/wns/HashChainGenerator.java
+++ src/com/ecem/wns/HashChainGenerator.java
@@ -6,26 +6,79 @@ import java.security.NoSuchAlgorithmException;
 
 public class HashChainGenerator {
 
-	/**
-	 * @param args
-	 * @throws NoSuchAlgorithmException 
-	 * @throws UnsupportedEncodingException 
-	 */
-	public static void main(String[] args) throws NoSuchAlgorithmException, UnsupportedEncodingException {
-		
-		int n = 5;
-		
+	private int n; /* array length */
+
+	private String[] forwardHashChain, backwardHashChain;
+
+	public HashChainGenerator(String forwardKey, String backwardKey, int n) {
+		this.n = n;
+
+		forwardHashChain = new String[n];
+		backwardHashChain = new String[n];
+
+		try {
+			generateForwardChain(forwardKey);
+		} catch (UnsupportedEncodingException | NoSuchAlgorithmException e) {
+			e.printStackTrace();
+		}
+		try {
+			generateBackwardChain(backwardKey);
+		} catch (NoSuchAlgorithmException | UnsupportedEncodingException e) {
+			e.printStackTrace();
+		}
+	}
+
+	private void generateForwardChain(String key)
+			throws UnsupportedEncodingException, NoSuchAlgorithmException {
+
 		MessageDigest md = MessageDigest.getInstance("SHA-256");
-		String text = "initialkey";
-		md.update(text.getBytes("UTF-8"));
+		md.update(key.getBytes("UTF-8"));
 		byte[] digest = md.digest();
-		
-		for(int i = 1; i < n; i++) {
-			
+		forwardHashChain[0] = toBinaryString(digest);
+
+		for (int i = 1; i < n; i++) {
 			md.update(digest);
 			digest = md.digest();
+			forwardHashChain[i] = toBinaryString(digest);
+		}
+	}
+
+	private void generateBackwardChain(String key)
+			throws NoSuchAlgorithmException, UnsupportedEncodingException {
+
+		MessageDigest md = MessageDigest.getInstance("SHA-256");
+		md.update(key.getBytes("UTF-8"));
+		byte[] digest = md.digest();
+		backwardHashChain[n - 1] = toBinaryString(digest);
+
+		for (int i = n - 2; i >= 0; i--) {
+			md.update(digest);
+			digest = md.digest();
+			backwardHashChain[i] = toBinaryString(digest);
+		}
+	}
+
+	public static String toBinaryString(byte[] digest) {
+
+		StringBuilder sb = new StringBuilder();
+
+		for (byte b : digest) {
+
+			String temp = Integer.toBinaryString(b);
+			int difference = 8 - temp.length();
+
+			if (difference < 0) {
+				sb.append(temp.subSequence(temp.length() - 9, temp.length() - 1));
+			} else {
+				while (difference > 0) {
+					sb.append(0);
+					difference--;
+				}
+				sb.append(temp);
+			}
 		}
 
+		return sb.toString();
 	}
 
 }
